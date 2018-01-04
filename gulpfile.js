1b3a04e238fd3f08028df59602c99cc048e8f8ac
ゆうきゃん <rgls.app@icloud.com>
2018-01-04 22:44:14 +0900

    EJS の導入

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+const gulp = require('gulp');
+const browserSync = require('browser-sync');
+const plumber = require('gulp-plumber');
+const notify = require('gulp-notify');
+const cache = require('gulp-cached');
+const ejs = require("gulp-ejs");
+const gutil = require('gulp-util');
+const reload = browserSync.reload;
+
+gulp.task('browser-sync', function () {
+    browserSync({
+        server: {
+            baseDir: './public'
+        },
+        open: 'external'
+    });
+});
+
+gulp.task('ejs', function () {
+    return gulp.src(
+        ["./resources/views/**/*.ejs", "!./resources/views/**/_*.ejs"]
+    )
+        .pipe(plumber({
+            errorHandler: notify.onError("Error: <%= error.message %>")
+        }))
+        .pipe(ejs({}, { "root": "./resources/views" }, { "ext": ".html" }).on('error', gutil.log))
+        .pipe(gulp.dest("./public"))
+});
+
+gulp.task('watch', function () {
+    gulp.watch(['./resources/**/*.ejs'], ['ejs', reload]);
+    gulp.watch('./public/css/**').on("change", reload);
+    gulp.watch('./public/js/**').on("change", reload);
+});
+
+gulp.task('default', ['browser-sync', 'watch', 'ejs']);
