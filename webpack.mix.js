fd135fdb17c7076beb2ac9128f5d7f1a41eab379
rinne_grid <Rinne.Grids@gmail.com>
2018-03-05 23:08:56 +0900

    Revert "refs #7 npm run watchでejsのビルドとbrowser-syncの起動ができるように暫定対応"
    This reverts commit d38568b2ba51d791a048056b4c4e220fd2e477e3.

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,10 +11,6 @@ require('laravel-mix-purgecss');
  | file for the application as well as bundling up all the JS files.
  |
  */
-const webpack           = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
-
 
 mix.setPublicPath('public')
   .js('resources/assets/js/app.js', 'js')
@@ -32,65 +28,3 @@ mix.setPublicPath('public')
     extensions: ['html', 'js', 'php', 'vue', 'ejs']
   })
   .copyDirectory('resources/assets/images', 'public/images');
-
-
-mix.webpackConfig({
-  module: {
-    rules: [
-      {
-        test: /\.ejs$/,
-        use: [
-          {
-            loader: 'extract-loader',
-          },
-          {
-            loader: 'html-loader',
-          },
-          {
-            loader: 'ejs-compiled-loader?htmlmin'
-          }
-        ] 
-      }
-    ]
-  },
-  plugins: [
-    new webpack.LoaderOptionsPlugin(
-      [
-        {
-          test: /\.ejs$/,
-          options: {
-            'ejs-compiled-loader': {
-              'htmlmin': true,
-              'htmlminOptions': {
-                removeComments: true
-              }
-            },
-          }
-        }
-      ]
-    ),
-    new HtmlWebpackPlugin({
-      inject: false,
-      filename: 'index.html',
-      template: 'resources/views/index.ejs'
-    }),
-    new HtmlWebpackPlugin({
-      inject: false,
-      filename: 'pages/contact_form/index.html',
-      template: 'resources/views/pages/contact_form/index.ejs'
-    }),
-    new HtmlWebpackPlugin({
-      inject: false,
-      filename: 'pages/holy_grail.html',
-      template: 'resources/views/pages/holy_grail.ejs'
-    }),
-    new BrowserSyncPlugin({
-      host: 'localhost',
-      server: { baseDir: ['public']},
-      files: [
-        'public/css/*.css',
-        'public/js/*.js'
-      ]
-    })
-  ]
-})
